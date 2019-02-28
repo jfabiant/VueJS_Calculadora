11289753c23c29078a54783fcd5473b5440f719c
John Fabian <john.timoteo@tecsup.edu.pe>
2019-02-28 12:27:14 -0500

    Actualizando calculadora con Vue.js

--- src/components/calculadora.vue
+++ src/components/calculadora.vue
@@ -6,42 +6,44 @@
     <input type="text" v-model="numero1">
     <input type="text" v-model="numero2">
     <button @click="operacion()">{{boton}}</button>
-    <center><h3> Resultado: {{resultado}}</h3></center>
+    <center>
+      <h3>Resultado: {{resultado}}</h3>
+    </center>
   </div>
 </template>
 <script>
 export default {
-
-props: ["titulo", "boton", "opc", "numero1", "numero2", "resultado"],
+  props: ["titulo", "boton", "opc"],
   name: "calculadora",
-  data: {
-     numero1: null,
-     numero2: null,
-     resultado: null
+  data() {
+    return {
+      numero1: null,
+      numero2: null,
+      resultado: null
+    };
   },
-  methods:{
-    operacion(){
-        // console.log(this.numero1);
-        // console.log(this.numero2);
-        if(this.opc==1){
-            //Sumar
-            const res = parseFloat(this.numero1) + parseFloat(this.numero2)
-            this.resultado = res;
-        } else if (this.opc==2) {
-            //Restar
-            let res = parseFloat(this.numero1) - parseFloat(this.numero2)
-            this.resultado = res;
-        } else if (this.opc==3) {
-            //Multiplicar
-            let res = parseFloat(this.numero1) * parseFloat(this.numero2)
-            this.resultado = res;
-        } else {
-            //Dividir
-            let res = parseFloat(this.numero1) / parseFloat(this.numero2);
-            this.resultado = res;
-        }
+  methods: {
+    operacion() {
+      // console.log(this.numero1);
+      // console.log(this.numero2);
+      if (this.opc == 1) {
+        //Sumar
+        const res = parseFloat(this.numero1) + parseFloat(this.numero2);
+        this.resultado = res;
+      } else if (this.opc == 2) {
+        //Restar
+        let res = parseFloat(this.numero1) - parseFloat(this.numero2);
+        this.resultado = res;
+      } else if (this.opc == 3) {
+        //Multiplicar
+        let res = parseFloat(this.numero1) * parseFloat(this.numero2);
+        this.resultado = res;
+      } else {
+        //Dividir
+        let res = parseFloat(this.numero1) / parseFloat(this.numero2);
+        this.resultado = res;
+      }
     }
   }
-
 };
 </script>
